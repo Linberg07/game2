16a199a268df0059621fd4575cb3fd1c19393e10
Linberg07 <63363265+Linberg07@users.noreply.github.com>
2021-09-21 21:33:11 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,55 @@
+function ibg(){
+	let ibg = document.querySelectorAll('.ibg');
+	for(var i=0;i<ibg.length;i++){
+		if(ibg[i].querySelector('img')){
+			ibg[i].style.backgroundImage = 'url('+ibg[i].querySelector('img').getAttribute('src')+')';
+		}
+	}
+
+}
+ibg();
+
+$(document).ready(function(){
+	$('.header-burger').click(function(){
+		$('.burger-menu').toggleClass('active');
+		$('body').toggleClass('lock');
+		$(this).toggleClass('active');
+	})
+	$('.burger-bg').click(function(){
+		$('.burger-menu').removeClass('active');
+		$('body').removeClass('lock');
+		$('.header-burger').removeClass('active');
+	})
+
+	var burger_height = $('.burger-menu').outerHeight();
+	jQuery.fn.reverse = function() {
+    return this.pushStack(this.get().reverse(), arguments);
+	}; 
+	var w = $(window).width();
+	if(w<768){
+		var headerLink = $('.header-top__link');
+		headerLink.each(function(){
+			$(this).appendTo($('.burger-menu__list'));
+		})
+	}else{
+		var headerLink = $('.header-top__link');
+		headerLink.reverse().each(function(){
+			$(this).prependTo($('.header-list'));
+		})
+	}
+})
+$(window).resize(function(){
+	var w = $(window).width();
+	if(w<768){
+		var headerLink = $('.header-top__link');
+		headerLink.each(function(){
+			$(this).appendTo($('.burger-menu__list'));
+		})
+	}else{
+		var headerLink = $('.header-top__link');
+		headerLink.reverse().each(function(){
+			$(this).prependTo($('.header-list'));
+		})
+	}
+})
+
